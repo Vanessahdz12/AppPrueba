f9572279dda0a325b4a522959369b31123f205a2
Vanessa Hernandez <hdzvanessa122019@hotmail.com>
2023-08-28 19:19:17 -0600

    Agregar archivos de proyecto.

---
+++ AppPruebaFinal/Datos/ClProcesarSQL.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace AppControldeIngresosCIMM.Datos
+{
+    public class ClProcesarSQL
+    {
+        //Ejecuta Consulta Select en forma desconectada y retorna DataTable
+        public DataTable mtdSelectDesc(string Consulta)
+        {
+
+            ClConexion objConexion = new ClConexion();
+            SqlDataAdapter adaptador = new SqlDataAdapter(Consulta, objConexion.mtdConexion());
+            DataTable tblDatos = new DataTable();
+            adaptador.Fill(tblDatos);
+            objConexion.mtdConexion().Close();
+            return tblDatos;
+        }
+
+        public DataTable CallExecProcedure(string procedure, SqlParameter[] parameters = null)
+        {
+            DataTable dataTable = new DataTable();
+            ClConexion conexion = new ClConexion();
+
+            using (SqlCommand command = new SqlCommand(procedure, conexion.mtdConexion()))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter(command))
+                {
+                    dataAdapter.Fill(dataTable);
+                }
+            }
+            return dataTable;
+        }
+
+        public SqlCommand mtdProcesoAlmacenado(string ProcesoAlmacenado)
+        {
+            ClConexion objConexion = new ClConexion();
+            SqlCommand comando = new SqlCommand(ProcesoAlmacenado, objConexion.mtdConexion());
+            comando.CommandType = CommandType.StoredProcedure;
+            return comando;
+        }
+      
+        public int mtdVerificarExistenciaCorreo(string consul)
+        {
+            ClConexion obConexion = new ClConexion();
+            SqlCommand comando = new SqlCommand(consul, obConexion.mtdConexion());
+            int verificar = (int)comando.ExecuteScalar();
+            obConexion.mtdConexion().Close();
+            return verificar;
+        }
+
+        public int mtdIUDConec(string consulta)
+        {
+            ClConexion objConexion = new ClConexion();
+            using (SqlConnection con = objConexion.mtdConexion())
+            {
+                SqlCommand comando = new SqlCommand(consulta, con);
+                int registro = comando.ExecuteNonQuery();
+                objConexion.mtdConexion().Close();
+                return registro;
+            }
+
+        }
+    }
+}
---
+++ AppPruebaFinal/Vista/ListadoPrograma.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Vista/Site1.Master" AutoEventWireup="true" CodeBehind="ListadoPrograma.aspx.cs" Inherits="AppPruebaFinal.Vista.ListadoPrograma" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container my-2">
+        <div class="row flex-shrink-1">
+            <table id="DataTablePrograma" class="table table-striped" style="width: 100%">
+                <thead>
+                    <tr>
+                        <th>Nombre_Programa</th>
+                        <th>Descripcion</th>
+                        <th>Ficha</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="modal fade venmodal" id="editarModal" tabindex="-1" role="dialog" aria-labelledby="editarModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editarModalLabel">Editar Registro</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <asp:TextBox ID="txtNombrePrograma" runat="server" placeholder="Documento" class="form-control mb-3 txt-Nombre-Programa"></asp:TextBox>
+                    <asp:TextBox ID="txtFicha" runat="server" placeholder="Nombre" class="form-control mb-3 txt-Ficha h-100"></asp:TextBox>
+                    <asp:TextBox ID="txtDescripcion" runat="server" placeHolder="Apellido" class="form-control mb-3 txt-Descripcion"></asp:TextBox>
+                </div>
+
+                <div class="modal-footer">
+                    <%--<asp:Button id="btnActualizar" class="btn btn-primary" runat="server" Text="Actualizar" />--%>
+                    <button id="btnActualizar" class="btn btn-primary" type="button">Actualizar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
